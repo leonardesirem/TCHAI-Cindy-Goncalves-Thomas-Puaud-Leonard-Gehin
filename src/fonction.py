59367e9a53b26275a76ddd2c5762fdfb20a5df65
Leonard Gehin <cuilliere169@gmail.com>
2019-11-19 10:00:46 +0100

    Creation de fonction.py et ajout des entetes.

---
+++ src/fonction.py
@@ -0,0 +1,21 @@
+
+def transaction():
+	return 0
+	
+def afficherListeTransaction():
+	return 0
+	
+def afficherListeTransactionDe():
+	return 0
+	
+def afficherSoldeCompte():
+	return 0
+
+def chargerDonnee():
+	return 0
+
+
+
+
+if __name__ == '__main__':
+	print("test")
